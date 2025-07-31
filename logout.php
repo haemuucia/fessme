b990adb4799d4033c1d2c2f1d986bb3bb1b77e03
Veronica Irene Tanumihadja <162682171+haemuucia@users.noreply.github.com>
2025-07-31 12:51:50 +0700

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,10 +1,8 @@
 <?php
-    // Hapus sesi login
     session_start();
     session_unset();
     session_destroy();
 
-    // Arahkan kembali ke halaman utama
     header("Location: login.php");
-    exit(); // Pastikan kode setelah header() tidak dijalankan
+    exit(); 
 ?>
